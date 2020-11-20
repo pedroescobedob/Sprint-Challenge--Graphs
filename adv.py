c3ad4d6922a56f0ce5c59e8f6eba2064e804d74b
Pedro Escobedo <pedro.escobedo.design@gmail.com>
2020-11-19 20:37:13 -0800

    mvp completed

--- adv.py
+++ adv.py
@@ -29,6 +29,104 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+graph = {}
+
+def opp_dir(dir):
+    if dir == 's':
+        return 'n'
+    if dir == 'n':
+        return 's'
+    if dir == 'e':
+        return 'w'
+    if dir == 'w':
+        return 'e'
+    
+def find_paths(room_exists):
+    exits = {}
+    for exit in room_exists:
+        exits[exit] = '?'
+    return exit
+
+def random_path(exits):
+    random_exit = exits[random.randint(0, len(exits) - 1)]
+    return random_exit
+
+exits = player.current_room.get_exits()
+
+direction = random_path(exits)
+
+unknown_exits = find_paths(exits)
+
+graph[player.current_room.id] = unknown_exits 
+
+# Traversal loop
+while len(room_graph) > len(graph): 
+
+    # Save room for later
+    last_room = player.current_room 
+
+    # Use random direction to move
+    player.travel(direction)
+    traversal_path.append(direction)
+
+    available_exits = {}
+
+    if player.current_room.id in graph:
+        available_exits = graph[player.current_room.id] 
+    else:
+        available_exits = find_paths(player.current_room.get_exits())
+
+    available_exits[opp_dir(direction)] = last_room.id
+    graph[player.current_room.id] = available_exits
+    graph[last_room.id][direction] = player.current_room.id
+
+    unknown = 0
+    unknown_paths = []
+
+    # Find all the directions that have not been explored on the current room
+    for exit,dire in graph[player.current_room.id].items(): 
+        if dire == '?':
+            unknown += 1
+            unknown_paths.append(exit)
+
+    if unknown == 0 or len(available_exits) == 1: 
+        qq= Queue()
+        visited = set()
+        qq.enqueue([(opp_dir(direction),last_room.id)])
+
+        while qq.size() > 0:
+            path = qq.dequeue()
+
+            temp = path[-1]
+            room_direct = temp[0]
+            room_id = temp[1]
+
+            if len(room_graph) == len(graph):
+                break
+
+            if room_id not in visited:
+                visited.add(room_id)
+
+                unexplored = None
+                for exit,direc in graph[room_id].items():
+                    if direc == '?':
+                        unexplored = exit
+                        break
+                    else:
+                        path_copy = path.copy()
+                        path_copy.append((exit,direc))
+                        qq.enqueue(path_copy)
+
+                if unexplored is not None:
+                    for item in path:
+                        player.travel(item[0])
+                        traversal_path.append(item[0])
+
+                    direction = unexplored
+                    break
+    else:
+        direction = random_path(unknown_paths)
+
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
